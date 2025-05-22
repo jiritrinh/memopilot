536ba403a62463d090b8954556e403d721daa9b1
Jiri Trinh <trnh.anhtu@gmail.com>
2025-05-22 21:50:06 +0200

    feat: add serializer for handling meeting endpoints

--- backend/meetings/serializers.py
+++ backend/meetings/serializers.py
@@ -5,4 +5,4 @@ class MeetingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Meeting
         fields = ['id', 'title', 'date', 'transcript', 'summary', 'user', 'created_at', 'updated_at']
-        read_only_fields = ['user']  # User will be set automatically 
+        read_only_fields = ['user']  
